09275d161320e41b79af817e37b80ad2952efaa4
Carlos Matos <carlos.1997matos@gmail.com>
2024-02-04 14:16:36 -0300

    Add DEV

---
+++ Dev/inferences/testefunc.py
@@ -0,0 +1,6 @@
+import os
+print(len("/home/carlos/Documentos/iniciacao_cientifica/Estudo_Yolo/dataset/carlos".split("/")[-1]))
+print(len("/home/carlos/Documentos/iniciacao_cientifica/Estudo_Yolo/dataset/carlos"))
+print("/home/carlos/Documentos/iniciacao_cientifica/Estudo_Yolo/dataset/carlos"[:-len("/home/carlos/Documentos/iniciacao_cientifica/Estudo_Yolo/dataset/carlos".split("/")[-1])])
+print("/home/carlos/Documentos/iniciacao_cientifica/Estudo_Yolo/dataset/carlos".split("/")[-1])
+#os.mkdir("/home/carlos/Documentos/iniciacao_cientifica/Estudo_Yolo/dataset/carlos")
